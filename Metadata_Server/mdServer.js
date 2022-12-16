91408130cd0fbc84ae7abde0a7fbed487e7f8b72
zhellouind <zac.hellouin@gmail.com>
2022-12-16 16:29:04 +0100

    Fixed metadata server and json response

--- Metadata_Server/mdServer.js
+++ Metadata_Server/mdServer.js
@@ -4,6 +4,8 @@ const port = 3000
 /* Add middleware that parses incoming json in req.body */
 app.use(express.json());
 
+const CODE_URL_BASE = "http://homepages.laas.fr/smedjiah/tmp/mw/"
+
 const conf = {
     localIp : "127.0.0.1",
     localPort : "",
@@ -11,7 +13,7 @@ const conf = {
     remoteIp : "",
     remotePort : "",
     remoteName : "",
-    codeURL : "http://homepages.laas.fr/smedjiah/tmp/mw/",
+    codeURL : "",
     deviceName : "",
     sendPeriod : ""
 };
@@ -25,39 +27,38 @@ const confServer = conf,
 /* Server Config */
 confServer.localPort = "8080";
 confServer.localName = "srv";
-confServer.codeURL = confServer.codeURL + "server.js";
+confServer.codeURL = CODE_URL_BASE + "server.js";
 /* Gateway Intermédiaire config */
 confGatewayI.localPort = "8181";
 confGatewayI.localName = "gwi";
 confGatewayI.remoteIp = confGatewayI.localIp;
 confGatewayI.remotePort = "8080";
 confGatewayI.remoteName = "srv";
-confGatewayI.codeURL = confGatewayI.codeURL + "gateway.js";
+confGatewayI.codeURL = CODE_URL_BASE + "gateway.js";
 /* Gateway F1 config */
 confGatewayF1.localPort = "8282";
 confGatewayF1.localName = "gwf1";
 confGatewayF1.remoteIp = "127.0.0.1";
 confGatewayF1.remotePort = "8181";
 confGatewayF1.remoteName = "gwi";
-confGatewayF1.codeURL = confGatewayF1 + "gateway.js";
+confGatewayF1.codeURL = CODE_URL_BASE + "gateway.js";
 /* Device Config */
 confDevice.localPort = "9001";
 confDevice.localName = "dev1";
 confDevice.remoteIp = "127.0.0.1";
 confDevice.remotePort = "8282";
 confDevice.remoteName = "gwf1";
-confDevice.codeURL = confDevice + "device.js";
+confDevice.codeURL = CODE_URL_BASE + "device.js";
 confDevice.sendPeriod = "3000";
 /* Application Config*/
 confApplication.remotePort = "8080";
-confApplication.codeURL = confApplication.codeURL + "application.js";
+confApplication.codeURL = CODE_URL_BASE + "application.js";
 confApplication.deviceName = "dev1";
 confApplication.sendPeriod = "5000";
 
 app.get('/getConf/:containerName', (req, res) => {
     const name = req.params.containerName;
-    console.log(`Detected Cont Name : ${name}`)
-    console.log(`ConfServer : ${confServer}`)
+    console.log(`Detected Req Cont Name : ${name}`)
     switch (name) {
         case 'Server':
             res.json(confServer);
