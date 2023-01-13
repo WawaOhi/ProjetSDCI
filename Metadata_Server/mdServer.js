085b64c53e0c388ac39283d19223f6b60d5c034f
zhellouind <zac.hellouin@gmail.com>
2023-01-13 11:21:10 +0100

    Md server provides type

--- Metadata_Server/mdServer.js
+++ Metadata_Server/mdServer.js
@@ -7,6 +7,7 @@ app.use(express.json());
 const CODE_URL_BASE = "http://homepages.laas.fr/smedjiah/tmp/mw/"
 
 const conf = {
+    type : "",
     localIp : "127.0.0.1",
     localPort : "",
     localName : "",
@@ -25,10 +26,12 @@ const confServer = conf,
     confApplication = conf;
 
 /* Server Config */
+confServer.type = "server"
 confServer.localPort = "8080";
 confServer.localName = "srv";
 confServer.codeURL = CODE_URL_BASE + "server.js";
 /* Gateway Intermédiaire config */
+confGatewayI.type = "gateway"
 confGatewayI.localPort = "8181";
 confGatewayI.localName = "gwi";
 confGatewayI.remoteIp = confGatewayI.localIp;
@@ -36,6 +39,7 @@ confGatewayI.remotePort = "8080";
 confGatewayI.remoteName = "srv";
 confGatewayI.codeURL = CODE_URL_BASE + "gateway.js";
 /* Gateway F1 config */
+confGatewayF1.type = "gateway"
 confGatewayF1.localPort = "8282";
 confGatewayF1.localName = "gwf1";
 confGatewayF1.remoteIp = "127.0.0.1";
@@ -43,6 +47,7 @@ confGatewayF1.remotePort = "8181";
 confGatewayF1.remoteName = "gwi";
 confGatewayF1.codeURL = CODE_URL_BASE + "gateway.js";
 /* Device Config */
+confDevice.type = "device"
 confDevice.localPort = "9001";
 confDevice.localName = "dev1";
 confDevice.remoteIp = "127.0.0.1";
@@ -51,6 +56,7 @@ confDevice.remoteName = "gwf1";
 confDevice.codeURL = CODE_URL_BASE + "device.js";
 confDevice.sendPeriod = "3000";
 /* Application Config*/
+confApplication.type = "application"
 confApplication.remotePort = "8080";
 confApplication.codeURL = CODE_URL_BASE + "application.js";
 confApplication.deviceName = "dev1";
