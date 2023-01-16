4129a121c9e3ecf4fbf6dc297797dc6683ada06f
WawaOhi <ouahi.walid@outlook.fr>
2023-01-16 09:54:46 +0100

    fichier init fait

--- generic_docker_image/init.sh
+++ generic_docker_image/init.sh
@@ -1,59 +1,37 @@
 #!/bin/sh 
 
 # Retrieve the json and store it in a json file
-curl http://172.17.0.2/getConf/$iam> $iam.json
+curl http://172.17.0.2:3000/getConf/$iam | jq '.'> $iam.json
 
+# Extraction des différents fields de notre fichier json 
+localIp=`cat $iam.json | jq -r '.localIp'`
+localPort=`cat $iam.json | jq -r '.localPort'`
+localName=`cat $iam.json | jq -r '.localName'`
+codeURL=`cat $iam.json | jq -r '.codeURL'`
+remotePort=`cat $iam.json | jq -r '.remotePort'`
+sendPeriod=`cat $iam.json | jq -r '.sendPeriod'`
+remoteIp=`cat $iam.json | jq -r '.remoteIp'`
+remoteName=`cat $iam.json | jq -r '.remoteName'`
 
-case $iam in 
-	"Server")
-		# Extraction des différents fields de notre fichier json
-		localPort =$(jq -r '.localPort' $iam.json)
-		localName =$(jq -r '.localName' $iam.json)
-		codeURL =$(jq -r '.codeURL' $iam.json)
-		export localPort
-		export localName
-		export codeURL
-		;;
-	"Application")
-		remotePort =$(jq -r '.remotePort' $iam.json)
-		codeURL =$(jq -r '.codeURL' $iam.json)
-		deviceName =$(jq -r '.deviceName' $iam.json)
-		sendPeriod =$(jq -r '.sendPeriod' $iam.json)
-		export remotePort
-		export codeURL
-		export deviceName
-		export sendPeriod
-		;;
-	"Device")
-		localPort =$(jq -r '.localPort' $iam.json)
-                localName =$(jq -r '.localName' $iam.json)
-                remoteIp =$(jq -r '.remoteIp' $iam.json)
-                remotePort =$(jq -r '.remotePort' $iam.json)
-		remoteName =$(jq -r '.remoteName' $iam.json)
-                codeURL =$(jq -r '.codeURL' $iam.json)
-		sendPeriod =$(jq -r '.sendPeriod' $iam.json)
-		export localPort
-		export localName
-		export remoteIp
-		export remotePort
-		export remoteName
-		export codeURL
-		export sendPeriod
-		;;
-	*)
-		# Extraction des différents fields de notre fichier json
-                localPort =$(jq -r '.localPort' $iam.json)
-                localName =$(jq -r '.localName' $iam.json)
-                remoteIp =$(jq -r '.remoteIp' $iam.json)
-		remotePort =$(jq -r '.remotePort' $iam.json)
-                remoteName =$(jq -r '.remoteName' $iam.json)
-                codeURL =$(jq -r '.codeURL' $iam.json)
-		export localPort
-		export localName
-		export remoteIp
-		export remotePort
-		export remoteName
-		export sendPeriod
-		;;
+#Type de composant utiisé. 
+type=`cat $iam.json | jq -r '.type'`
+
+#récupération du fichier js grace à un wget
+wget $codeURL
+
+#En fonction du type, on execute la commande appropriée
+case $type in
+    gateway)
+        node gateway.js --local_ip $localIp --local_port $localPort --local_name $localName --remote_ip $remoteIp --remote_port $remotePort --remote_name $remoteName
+        ;;
+    server)
+        node server.js --local_ip $localIp --local_port $localPort --local_name $localName
+        ;;
+    device)
+        node device.js --local_ip $localIp --local_port $localPort --local_name $localName --remote_ip $remoteIp --remote_port $remotePort --remote_name $remoteName --send_period $sendPeriod
+        ;;
+    *)
+        node application.js --remote_ip $localIp --remote_port $remotePort --device_name $localName --send_period $sendPeriod
+        ;;
 esac
 
