a87181723e9cb96e33b97991b0581b3a6f0598ef
zhellouind <zac.hellouin@gmail.com>
2023-01-23 16:47:39 +0100

    updated monitoring.py

--- vnfs/vnf_monitoring_gwi/monitoring.py
+++ vnfs/vnf_monitoring_gwi/monitoring.py
@@ -1,24 +1,25 @@
 import collections
 
 from flask import Flask
-from apscheduler.schedulers.background import BackgroundScheduler, BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 import time
+import requests
 
 app = Flask(__name__)
 
 GWI_IP = '10.0.0.2'
+GWI_PORT = '8181'
+GWI_URL = 'http://' + GWI_IP + ':' + GWI_PORT
 
 MAX_LEN_STATES_DEQUE = 15
 MAX_LEN_PING_DEQUE = 15
 COLLECT_STATES_FREQ_S = 1
 COLLECT_PING_FREQ_S = 15
+STATE_REQ_TIMEOUT = 0.5
+PING_REQ_TIMEOUT = 0.5
 
 states = collections.deque([], maxlen=MAX_LEN_STATES_DEQUE)
 pings = collections.deque([], maxlen=MAX_LEN_PING_DEQUE)
-
-# @app.route("/state")
-# def provide_state():
-#     return list(states)
 
 
 @app.route("/state", defaults={'list_len': MAX_LEN_STATES_DEQUE})
@@ -43,20 +44,47 @@ def provide_ping(list_len: int):
 
 @app.route("/all")
 def provide_all():
-
-    return 'PING'
+    return {'pings': list(pings), 'states': list(states)}
 
 
 def collect_state():
+    try:
+        r = requests.get(GWI_URL + '/health', timeout=STATE_REQ_TIMEOUT)
+        state = r.json()
+    except requests.exceptions.Timeout:
+        state = [{'error': 'timeout'}]
+    except requests.exceptions.TooManyRedirects:
+        state = [{'error': 'badURL'}]
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
+
+    state.append({'timestamp': time.time()})
     current_time = time.strftime("%H:%M:%S", time.localtime())
-    # print(f'Health request at {current_time}')
-    states.append(f'HEALTH at {current_time}')
-    print(f'STATES: {states}')
+    print(f'Health request at {current_time} gave state {state}')
+
+    states.append(state)
+
+    print(f'STATES: {list(states)}')
 
 
 def collect_ping():
-    # print(f'Ping request at {time.strftime("%H:%M:%S", time.localtime())}')
-    print()
+    try:
+        r = requests.get(GWI_URL + '/ping', timeout=PING_REQ_TIMEOUT)
+        ping = r.json()
+    except requests.exceptions.Timeout:
+        ping = [{'error': 'timeout'}]
+    except requests.exceptions.TooManyRedirects:
+        ping = [{'error': 'badURL'}]
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
+
+    ping.append({'timestamp': time.time()})
+
+    current_time = time.strftime("%H:%M:%S", time.localtime())
+    print(f'Ping request at {current_time} gave ping {ping}')
+
+    pings.append(ping)
+    print(f'Pings: {pings}')
 
 
 scheduler = BackgroundScheduler()
